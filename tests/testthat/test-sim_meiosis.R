6bfe7b6f1483f445a9723c230f581eabac2cf205
Karl Broman <kbroman@gmail.com>
2014-09-19 22:14:41 -0500

    tests of sim_crossovers, meiosis, and cross

--- tests/testthat/test-sim_meiosis.R
+++ tests/testthat/test-sim_meiosis.R
@@ -1,7 +1,7 @@
 
 context("sim_meiosis")
 
-test_that("test of create_parent", {
+test_that("create_parent works", {
 
     expected_p1 <- list(mat=data.frame(alleles=1L, locations=100.0),
                         pat=data.frame(alleles=1L, locations=100.0))
@@ -34,7 +34,7 @@ test_that("test of create_parent", {
 })
 
 
-test_that("test of check_individual", {
+test_that("check_individual works", {
 
     expected_p1 <- list(mat=data.frame(alleles=1L, locations=100.0),
                         pat=data.frame(alleles=1L, locations=100.0))
@@ -82,3 +82,61 @@ test_that("test of check_individual", {
     expect_error(check_individual(z))
 })
 
+test_that("simulations of meiosis and crosses work", {
+
+    # these are all rather contrived, but it's a start
+
+    seed <- 79251218
+    set.seed(seed)
+
+    expected <- c(92.82105867750943, 141.69451058842242, 283.80087793339044)
+    expect_equal(sim_crossovers(300, 10, 0.3), expected)
+
+    allele <- ifelse(runif(1) < 0.5, 1L, 2L)
+    expect_equal(allele, 1L)
+
+    another_set <- sim_crossovers(300, 3, 0.01)
+    another_allele <- ifelse(runif(1) < 0.5, 1L, 2L)
+    expect_equal(another_allele, 2L)
+
+    p1 <- create_parent(300, 1)
+    p2 <- create_parent(300, 2)
+    f1 <- cross(p1, p2)
+    expect_equal(create_parent(300, 1:2), f1)
+
+    set.seed(seed)
+    expected2 <- data.frame(alleles=c(1L, 2L, 1L, 2L), locations=c(expected, 300))
+    expect_equal(meiosis(f1, m=10, p=0.3), expected2)
+
+    expected3 <- data.frame(alleles=c(2L, 1L, 2L), locations=c(another_set, 300))
+    expect_equal(meiosis(f1, m=3, p=0.01), expected3)
+
+    set.seed(seed)
+    f2 <- cross(f1, f1, m=10, p=0.3)
+    expected <- list(mat=expected2,
+                     pat=data.frame(alleles=c(1L, 2L), locations=c(124.580098665319, 300)))
+    expect_equal(f2, expected)
+
+    set.seed(seed)
+    junk <- meiosis(f1, m=10, p=0.3)
+    f2 <- cross(f1, f1, m=3, p=0.01)
+    expected <- list(mat=expected3,
+                     pat=data.frame(alleles=c(2L, 1L, 2L, 1L),
+                     locations=c(130.79544918146, 201.651784661226, 251.381423673593, 300)))
+    expect_equal(f2, expected)
+
+    set.seed(seed)
+    p1 <- create_parent(300, 1:2)
+    p2 <- create_parent(300, 3:4)
+    f1a <- cross(p1, p2)
+    f2a <- cross(p1, p2)
+    sib <- cross(f1a, f2a)
+    expected <- list(mat=data.frame(alleles=c(3L, 1L, 2L, 3L, 4L, 2L, 1L, 4L),
+                                    locations=c(49.9227490508929, 115.871118125506, 146.964924945496, 173.550174990669,
+                                                187.015442247503, 211.255158483982, 262.606337293983, 300.0)),
+                     pat=data.frame(alleles=c(2L, 1L, 4L, 3L, 1L, 2L, 3L, 4L),
+                                    locations=c(9.019920299761, 16.7069100541994, 21.4588083559647, 73.5785636818036,
+                                                167.808948992752, 186.267593340017, 283.901682123542, 300.0)))
+    expect_equal(sib, expected)
+
+})

b325d96226fade43ffe40e61afeda8d30be854ee
Karl Broman <kbroman@biostat.wisc.edu>
2014-09-26 07:56:26 -0500

    random.cpp: vector version of random_int

--- src/random.cpp
+++ src/random.cpp
@@ -7,3 +7,13 @@ int random_int(const int low, const int high)
     return (int)R::runif((double)low, double(high+1));
 }
 
+// vector of random integers from {low, low+1, ..., high}
+IntegerVector random_int(const int n, const int low, const int high)
+{
+    IntegerVector result(n);
+
+    for(int i=0; i<n; i++)
+        result[i] = random_int(low, high);
+    
+    return(result);
+}

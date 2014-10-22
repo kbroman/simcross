46552692d987c23df93f4acc4d53c01f00339b0b
Jianan Tian <jiant17@gmail.com>
2014-10-21 22:27:47 -0500

    add functions to simulate xos and convert to genos for multiple chromosomes.

---
+++ R/sim_from_pedigree_wholechr.R
@@ -0,0 +1,36 @@
+#' Simulate genotypes for pedigree for multiple chromosomes
+#'
+#' Simulate genotypes along all chromosomes for a pedigree. This is a
+#' wrap up of sim_from_pedigree.
+#' 
+#' @inheritParamss sim_from_pedigree
+#' @param map marker locations, a list with elements for each
+#' chromosome
+#' 
+#' @return A list with each component being the result from
+#' \code{sim_from_pedigree}, of length same as \code{map}.
+#' 
+#' @export
+#' @keywords datagen
+#' @seealso \code{\link{check_pedigree}},
+#' \code{\link{sim_ril_pedigree}}, \code{\link{sim_ail_pedigree}}
+#' \code{\link{sim_from_pedigree}}
+#'
+#' @examples
+#' # marker map
+#' map <- sim.map(len=rep(100, 19), n.mar=10, include.x=FALSE)
+#' # simulate AIL pedigree
+#' tab <- sim_ail_pedigree(12, 30)
+#' # simulate data from that pedigree
+#' dat <- sim_from_pedigree_wholechr(tab, map)
+#' 
+sim_from_pedigree_wholechr <- function(pedigree, map, m=10, p=0, obligate_chiasma=FALSE){
+  chr.len <- sapply(map, max)
+  is.xchr <- sapply(map, class) == "X"
+  dat <- NULL
+  for(chr in 1:length(map))
+      dat[[chr]] <- sim_from_pedigree(pedigree, L=chr.len[chr], xchr=is.xchr[chr],
+                                      m=m, p=p, obligate_chiasma=obligate_chiasma)
+  names(dat) <- names(map)
+  return(dat)
+}

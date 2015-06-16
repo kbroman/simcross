8c6f22511882b17debc76dabc2f2b0ddf4be409b
Karl Broman <kbroman@gmail.com>
2015-06-16 13:06:08 -0500

    sim_from_pedigree_wholechr: remove trailing whitespace

--- R/sim_from_pedigree_wholechr.R
+++ R/sim_from_pedigree_wholechr.R
@@ -2,14 +2,14 @@
 #'
 #' Simulate genotypes along all chromosomes for a pedigree. This is a
 #' wrap up of sim_from_pedigree.
-#' 
+#'
 #' @inheritParams sim_from_pedigree
 #' @param map marker locations, a list with elements for each
 #' chromosome
-#' 
+#'
 #' @return A list with each component being the result from
 #' \code{sim_from_pedigree}, of length same as \code{map}.
-#' 
+#'
 #' @export
 #' @keywords datagen
 #' @seealso \code{\link{check_pedigree}},
@@ -24,7 +24,7 @@
 #' tab <- sim_ail_pedigree(12, 30)
 #' # simulate data from that pedigree
 #' dat <- sim_from_pedigree_wholechr(tab, map)
-#' 
+#'
 sim_from_pedigree_wholechr <- function(pedigree, map, m=10, p=0, obligate_chiasma=FALSE){
   chr.len <- sapply(map, max)
   is.xchr <- sapply(map, class) == "X"
